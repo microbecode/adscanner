d95ccb809654888f2f4b4019e12701aa70fb4abc
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2020-08-31 14:05:45 +0300

    Project structure, initial

---
+++ AdScanner/Trigger.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace AdScanner
+{
+    public static class Trigger
+    {
+        [FunctionName(nameof(Trigger))]
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+
+        }
+    }
+}
